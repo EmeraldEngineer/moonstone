2cbfd8eab0986385928a2f72dc5c9231d80ead77
Taylor McCarthy <oresshi@gmail.com>
2017-02-15 10:12:05 -0700

    added app.component

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+
+import {Component} from "@angular/core";
+
+@Component({
+	// Update selector with YOUR_APP_NAME-app. This needs to match the custom tag in webpack/index.php
+	selector: 'moonstone-app',
+
+	// templateUrl path to your public_html/templates directory.
+	templateUrl: './templates/moonstone-app.php'
+})
+
+export class AppComponent {}
